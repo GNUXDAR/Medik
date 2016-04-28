933465ab392cb0376422e2e1ff954de3c6d8349b
grebo87 <grebo87@gmail.com>
2016-04-27 20:50:50 -0430

    que peo

--- control/conexion.php
+++ control/conexion.php
@@ -2,7 +2,7 @@
 //MODULO DE CONEXION ORIENTADA A OBJETOS
 class Conection {
 	public function conectar(){
-		$CONECTAR="host='127.0.0.1' dbname='consultorio' user='canaima' password='canaima'";
+		$CONECTAR="host='127.0.0.1' dbname='consulta' user='grebo' password='123'";
 		$CONEXION=pg_connect($CONECTAR);
 
 		if ($CONEXION==NULL) {
