7a12055293ba2bf4076b63dd5214dc62a8adc0e9
grebo87 <grebo87@gmail.com>
2016-04-28 08:49:07 -0430

    emitir alerta de las citas programadas para el dia

--- vistas/principal.php
+++ vistas/principal.php
@@ -7,7 +7,12 @@
         include_once('../control/conexion.php');
         include_once('sidebar.php');
         include_once('script.php');
+        $fecha = date('Y-m-d');
+        $buscarCitas="SELECT * FROM  cita_cnslt INNER JOIN pacnt_cnslt ON (cita_cnslt.ci_pacnt_cita = pacnt_cnslt.ci_pacnt) WHERE fecha_cita = '$fecha'";
+        $conectando = new Conection();
 
+        $listaCitas = pg_query($conectando->conectar(), $buscarCitas) or die('ERROR AL BUSCAR DATOS: ' . pg_last_error());
+        $resul = pg_fetch_all($listaCitas);
 ?>  
     <div class="content">
         <div id="pad-wrapper" class="form-page">
@@ -19,7 +24,7 @@
 
             <div class="row">
                 <div class="col col-4">
-                    <a  href="listas_citas.php" class="btn btn-default">Citas del Dia</a>
+                    <a  href="listas_citas.php" class="btn btn-info">Citas del Dia <span class="badge"><?php echo pg_num_rows($listaCitas); ?></span></a>
                 </div>
             
             </div>
