f9bf6ef146631f12f9e87276e3222c8204d82e2d
grebo87 <grebo87@gmail.com>
2016-04-27 21:14:55 -0430

    jj

--- vistas/user_create.php
+++ vistas/user_create.php
@@ -15,7 +15,7 @@
 
 
        <form method="POST" id="from_user" class="form-horizontal" action="../control/reg_user.php" autocomplete="off">
-                       
+                     
                         <div class="form-group">
                                 <label class="control-label col-xs-2">Nombre:</label>
                                 <div class="col-md-7">
@@ -84,7 +84,7 @@
                             
                         </div> 
                         
-                        
+                      
                     </form>
         
 </div>
@@ -157,7 +157,7 @@ $("#from_user").validate({
 
 <style type="text/css">
     
-    #from_user label.error {
+#from_user label.error {
         margin-left: 10px;
         width: auto;
         display: inline;
@@ -166,5 +166,32 @@ $("#from_user").validate({
     .error {
         text-decoration-color: red;
         color: red;
-    }
+    } 
 </style>
+<form class="cmxform" id="commentForm" method="get" action="">
+  <fieldset>
+    <legend>Please provide your name, email address (won't be published) and a comment</legend>
+    <p>
+      <label for="cname">Name (required, at least 2 characters)</label>
+      <input id="cname" name="name" minlength="2" type="text" required>
+    </p>
+    <p>
+      <label for="cemail">E-Mail (required)</label>
+      <input id="cemail" type="email" name="email" required>
+    </p>
+    <p>
+      <label for="curl">URL (optional)</label>
+      <input id="curl" type="url" name="url">
+    </p>
+    <p>
+      <label for="ccomment">Your comment (required)</label>
+      <textarea id="ccomment" name="comment" required></textarea>
+    </p>
+    <p>
+      <input class="submit" type="submit" value="Submit">
+    </p>
+  </fieldset>
+</form>
+<script>
+$("#commentForm").validate();
+</script>
