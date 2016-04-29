a417991de0d187bb4bd227836a420c33791e63d3
grebo87 <grebo87@gmail.com>
2016-04-28 20:44:49 -0430

    se agrego

---
+++ vistas/js/app.js
@@ -0,0 +1,34 @@
+// ========================================================================
+//  select2
+// ========================================================================
+$(function () {
+    $('select2').select2();
+});
+
+// ========================================================================
+//  DataTable
+// ========================================================================
+$(function () {
+    $(".dataTable").DataTable( {
+            "language": {
+                    "sProcessing":     "Procesando...",
+                    "sLengthMenu":     "Mostrar _MENU_ registros",
+                    "sZeroRecords":    "No se encontraron resultados",
+                    "sEmptyTable":     "Ningún dato disponible en esta tabla",
+                    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+                    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                    "sInfoPostFix":    "",
+                    "sSearch":         "Buscar:",
+                    "sUrl":            "",
+                    "sInfoThousands":  ",",
+                    "sLoadingRecords": "Cargando...",
+                    "oPaginate": {
+                        "sFirst":    "Primero",
+                        "sLast":     "Último",
+                        "sNext":     "Siguiente",
+                        "sPrevious": "Anterior"
+                    },
+            }
+    } );
+});
