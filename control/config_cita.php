7a7bb1bbbbe11b32e3dc30c70115c83d9d4f00b5
grebo87 <grebo87@gmail.com>
2016-05-04 12:08:06 -0430

    update

--- control/config_cita.php
+++ control/config_cita.php
@@ -7,7 +7,7 @@ $conectando = new Conection();
 $fecha_config = date('Y-m-d');
 $status = 1;
 $sql = '';
-if ($_POST['guardar_precio_cita']) {
+if (@$_POST['guardar_precio_cita']) {
 	$tipo = 1;
 	$precio = $_POST['precio_cita'];
 	$sql="SELECT * FROM  config_cita WHERE status = '1' AND tipo = $tipo ";
@@ -33,8 +33,34 @@ if ($_POST['guardar_precio_cita']) {
 		
 }//FIN if ($_POST['guardar_precio_cita'])
 
-if ($_POST['guardar_numero_cita']) {
+if (@$_POST['guardar_numero_cita']) {
 	$tipo = 2;
 	$numero = $_POST['numero_cita'];
+	$sql="SELECT * FROM  config_cita WHERE status = '1' AND tipo = $tipo ";
+	$query = pg_query($conectando->conectar(), $sql) or die('ERROR AL BUSCAR DATOS: ' . pg_last_error());
+	if (pg_num_rows($query) > 0) {
+		$sql="UPDATE config_cita SET numero_cita = '$numero' WHERE status = '1' AND tipo = $tipo";
+		$query = pg_query($conectando->conectar(), $sql) or die('ERROR AL BUSCAR DATOS: ' . pg_last_error());
+		if (pg_affected_rows($query) > 0) {
+				print ("<script>alert('Número de Citas Agregado');</script>");
+		    	print('<meta http-equiv="refresh" content="0; URL=../vistas/config_citas.php">');
+			} else {
+				print ("<script>alert('Número de Citas no Agregado');</script>");
+		    	print('<meta http-equiv="refresh" content="0; URL=../vistas/config_citas.php">');
+			}
+				
+	}else{
+		$INSERTAR=pg_query($conectando->conectar(), "INSERT INTO config_cita (numero_cita, tipo, fecha_config, status, id_usr)
+		VALUES ('$numero', '$tipo', '$fecha_config', '$status', '$id_usr')");
+		if ($INSERTAR) {
+				print ("<script>alert('Número de Citas Agregado');</script>");
+		    	print('<meta http-equiv="refresh" content="0; URL=../vistas/config_citas.php">');
+		} else {
+				print ("<script>alert('Número de Citas no Agregado');</script>");
+		    	print('<meta http-equiv="refresh" content="0; URL=../vistas/config_citas.php">');
+		}
+		
+	}
+	exit();
 }
 
